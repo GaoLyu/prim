238819328e3958e5378362175746e04ff674e060
Gaoyueyang Lyu <gaoyueyang.lyu@gmail.com>
2023-03-24 08:10:34 -0400

    Update graph.c

--- graph.c
+++ graph.c
@@ -12,47 +12,46 @@
 /* Returns a newly created Edge from vertex with ID 'fromVertex' to vertex
  * with ID 'toVertex', with weight 'weight'.
  */
-Edge * newEdge(int fromVertex, int toVertex, int weight) {
-  Edge * new = malloc(sizeof(Edge));
-  new -> fromVertex = fromVertex;
-  new -> toVertex = toVertex;
-  new -> weight = weight;
+Edge* newEdge(int fromVertex, int toVertex, int weight) {
+  Edge* new = malloc(sizeof(Edge));
+  new->fromVertex = fromVertex;
+  new->toVertex = toVertex;
+  new->weight = weight;
   return new;
 }
 
 /* Returns a newly created AdjList containing 'edge' and pointing to the next
  * AdjList node 'next'.
  */
-AdjList * newAdjList(Edge * edge, AdjList * next) {
-  AdjList * new = calloc(1, sizeof(AdjList));
-  new -> edge = edge;
-  new -> next = next;
+AdjList* newAdjList(Edge* edge, AdjList* next) {
+  AdjList* new = calloc(1, sizeof(AdjList));
+  new->edge = edge;
+  new->next = next;
   return new;
 }
 
 /* Returns a newly created Graph with space for 'numVertices' vertices.
  * Precondition: numVertices >= 0
  */
-Graph * newGraph(int numVertices) {
-  Graph * new = malloc(sizeof(Graph));
-  new -> numVertices = numVertices;
-  new -> numEdges = 0;
-  new -> vertices = malloc(sizeof(Vertex) * numVertices);
+Graph* newGraph(int numVertices) {
+  Graph* new = malloc(sizeof(Graph));
+  new->numVertices = numVertices;
+  new->numEdges = 0;
+  new->vertices = malloc(sizeof(Vertex) * numVertices);
   for (int i = 0; i < numVertices; i++) {
-    new -> vertices[i].id = i;
-    new -> vertices[i].adjList = NULL;
+    new->vertices[i].id = i;
+    new->vertices[i].adjList = NULL;
   }
-  // intialize??????????????????
   return new;
 }
 
 /* Frees memory allocated for AdjList starting at 'head'.
  */
-void deleteAdjList(AdjList * head) {
-  AdjList * next;
+void deleteAdjList(AdjList* head) {
+  AdjList* next;
   while (head != NULL) {
-    next = head -> next;
-    free(head -> edge);
+    next = head->next;
+    free(head->edge);
     free(head);
     head = next;
   }
@@ -60,17 +59,15 @@ void deleteAdjList(AdjList * head) {
 
 /* Frees memory allocated for 'vertex''s adjacency list.
  */
-void deleteVertex(Vertex * vertex) {
-  deleteAdjList(vertex -> adjList);
-}
+void deleteVertex(Vertex* vertex) { deleteAdjList(vertex->adjList); }
 
 /* Frees memory allocated for 'graph'.
  */
-void deleteGraph(Graph * graph) {
-  for (int i = 0; i < graph -> numVertices; i++) {
-    deleteVertex( & (graph -> vertices[i]));
+void deleteGraph(Graph* graph) {
+  for (int i = 0; i < graph->numVertices; i++) {
+    deleteVertex(&(graph->vertices[i]));
   }
-  free(graph -> vertices);
+  free(graph->vertices);
   free(graph);
 }
 
@@ -78,33 +75,33 @@ void deleteGraph(Graph * graph) {
  ** Helper function provided in the starter code
  *********************************************************************/
 
-void printEdge(Edge * edge) {
+void printEdge(Edge* edge) {
   if (edge == NULL) return;
-  printf("(%d -- %d, %d)", edge -> fromVertex, edge -> toVertex, edge -> weight);
+  printf("(%d -- %d, %d)", edge->fromVertex, edge->toVertex, edge->weight);
 }
 
-void printAdjList(AdjList * head) {
+void printAdjList(AdjList* head) {
   while (head != NULL) {
-    printEdge(head -> edge);
+    printEdge(head->edge);
     printf("  ");
-    head = head -> next;
+    head = head->next;
   }
 }
 
-void printVertex(Vertex * vertex) {
+void printVertex(Vertex* vertex) {
   if (vertex == NULL) return;
-  printf("%d: ", vertex -> id);
-  printAdjList(vertex -> adjList);
+  printf("%d: ", vertex->id);
+  printAdjList(vertex->adjList);
 }
 
-void printGraph(Graph * graph) {
+void printGraph(Graph* graph) {
   if (graph == NULL) return;
 
-  printf("Number of vertices: %d. Number of edges: %d.\n\n", graph -> numVertices,
-    graph -> numEdges);
+  printf("Number of vertices: %d. Number of edges: %d.\n\n", graph->numVertices,
+         graph->numEdges);
 
-  for (int i = 0; i < graph -> numVertices; i++) {
-    printVertex( & graph -> vertices[i]);
+  for (int i = 0; i < graph->numVertices; i++) {
+    printVertex(&graph->vertices[i]);
     printf("\n");
   }
   printf("\n");
